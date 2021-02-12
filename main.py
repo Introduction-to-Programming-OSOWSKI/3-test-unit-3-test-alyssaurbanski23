dfa7c7ee8050a5dad9eec6fa89a17a4a5b4341db
alyssaurbanski23 <831554@mystma.org>
2021-02-12 02:15:42 +0000

    unit 3 test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#WRITE YOUR CODE IN THIS FILE
+def countB(b):
+    if i in range (b == 1):
+        b is 1
+print(countB(bolloon))
+
+def removeLast(e):
+    if i in range (e ==1):
+        e is 1
+print(removeLast(same))
+
+def sumBetweenOdd(x, y):
+    if i in range (6 == 7):
+        6 is True
+print(sumBetweenOdd(6, 8))
+
+def firstLast(word):
+    if word == 6:
+        6 is True
+print(firstLast(snail))
+
